3a38e0b58f261b6673350bb1920e242d9ab20082
Tommy <swasin185@gmail.com>
2025-12-05 18:05:59 +0700

    vercel-edge

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -4,6 +4,10 @@ const route = useRoute()
 const { data: page } = await useAsyncData('page-' + route.path, () => {
   return queryCollection('content').path(route.path).first()
 })
+
+if (!page.value) {
+  throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
+}
 </script>
 
 <template>
