95ced17297455663fbef059a704a8a58bfb9bec2
Tommy <swasin185@gmail.com>
2025-12-05 18:48:30 +0700

    preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2024-04-03',
   nitro: {
-    preset: 'vercel-edge',
+    preset: 'vercel',
     prerender: {
       crawlLinks: true
     }
