2e24c2358f8dd2ac8ef5d19faed67570d8ed1642
Tommy <swasin185@gmail.com>
2025-12-05 18:42:29 +0700

    nitro

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -5,14 +5,11 @@ const { data: page } = await useAsyncData('page-' + route.path, () => {
   return queryCollection('content').path(route.path).first()
 })
 
-if (!page.value) {
-  throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
-}
+// if (!page.value) {
+//   throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
+// }
 </script>
 
 <template>
-  <ContentRenderer
-    v-if="page"
-    :value="page"
-  />
+  <ContentRenderer :value="page!" />
 </template>
