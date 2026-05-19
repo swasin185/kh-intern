712a447b03400f947a3513dc23e47b3af6b31f8b
swasin185 <swasin185@gmail.com>
2026-05-19 09:29:31 +0700

    pnpm update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   ssr: true,
   extends : "docus",
-  modules: ["nuxt-studio"],
+  modules: [],
   devtools: { enabled: false },
   compatibilityDate: "2024-04-03",
   nitro: {
